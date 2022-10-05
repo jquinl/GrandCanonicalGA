dff397cad0af33a9f9a1248ab30365e9e3aaea04
jon <jon@office>
2022-10-05 10:55:38 +0200

    BugfixesLammps

--- GCGA/GCGA.py
+++ GCGA/GCGA.py
@@ -40,7 +40,7 @@ class GCGA:
                 initial_structure_generator = RCG, crossing_operator = CO, stc_change_operator = CHG,
                 mutations = None,mutation_chance=0.3,
                 steps = 1000,maxtries = 10000,
-                restart=False,restart_filename=None, force_minimization_lammps = True
+                restart=False,restart_filename=None,
                 ):
 
         #--------Population settings----------------
@@ -93,7 +93,6 @@ class GCGA:
         self.restart_filename = restart_filename
 
         self.evalnum = 0
-        self.forcemin = force_minimization_lammps
         
 
 #--------Functions only called during initialization---------------
@@ -387,13 +386,9 @@ class GCGA:
             results = {'energy': E,'forces': F}
         elif(isinstance(self.calc,LAMMPS)):
             try:
-                lammps_atoms= copy.deepcopy(atoms)
+                lammps_atoms=atoms.copy()
                 lammps_atoms.set_calculator(self.calc)
                
-                if(self.forcemin):
-                    while(np.amax(np.abs(lammps_atoms.get_forces()))>0.1):
-                        lammps_atoms = copy.deepcopy(lammps_atoms)
-                        lammps_atoms.set_calculator(self.calc)
                 E = lammps_atoms.get_potential_energy()
                 F = lammps_atoms.get_forces()
                 results = {'energy': E,'forces': F}
